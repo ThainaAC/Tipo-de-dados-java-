7ae3932da529bfd5b5c3d23afa4d693f258cada1
ThainaAC <alvesthaina568@gmail.com>
2023-02-14 14:13:48 +0000

    salvar

--- EstruturaDeSelecao.java
+++ EstruturaDeSelecao.java
@@ -1,3 +1,14 @@
 public class EstruturaDeSelecao {
+    public static void main(String[] args){
+        int idade;
+        idade=19;
+        if (idade >=18)
+        {
+            System.out.print( "Apto ao Serviço Nilitar O brigatório!");
+
+        } else {
+            System.out.print("Não esta apto ao Serviço Militar Obrigatório!");
+        }
+    }
     
 }
