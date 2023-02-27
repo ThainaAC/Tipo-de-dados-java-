4936a368b93a565da105677d7a264a45e8fa07ad
ThainaAC <alvesthaina568@gmail.com>
2023-02-27 11:24:02 +0000

    salvar

---
+++ MediaDeNotas.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class MediaDeNotas {
+    
+    public static void main(String[] args){
+        Scanner input = new Scanner (System.in);
+        int n1, n2, media;
+
+    System.out.println("Informe o valor da primeira nota: ");
+    n1 = input.nextInt();
+    
+    System.out.println("Informe o valor da segunda nota: ");
+    n2 = input.nextInt();
+
+    media = (n1 + n2) /2 ;
+    
+    System.out.printf("Sua primeiro nota foi = " + n1);
+    System.out.printf(" \n Sua segundo nota foi foi  = " + n2);
+    System.out.printf(" \n E sua media é = " + media + "\n");   
+    
+    if (media >= 8 && <= 10 )
+    {
+        System.out.print("Aluno está aprovado \n");
+    } else if (media >= 5 && media <= 7){
+        System.out.print("Aprovado, mas tera que fazer recuperação \n");
+    } else if (media >= 0 && <= 4) {
+        System.out.print("Aluno está reprovado \n");
+    } else {
+        System.out.print("erro na media \n");
+    }
+}
+}
