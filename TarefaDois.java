ba2ada7120508c332dd868754125502357d283d9
ThainaAC <alvesthaina568@gmail.com>
2023-03-07 14:11:57 +0000

    salvar

--- TarefaDois.java
+++ TarefaDois.java
@@ -13,15 +13,29 @@ public class TarefaDois
 
         System.out.println("Informe sua altura: ");
         Altura = input.nextDouble();
-        
-        
-        
+    
         imc = Peso / (Altura * Altura);
-
-        System.out.printf(" \n O seu peso é: = " + Peso + "\n");    
-        System.out.printf("Seu primeiro numero foi = " + Altura + "\n");
-        System.out.printf(" \n Seu segundo numero foi  = " + imc + "\n");
         
+        if (Peso < 18.5){
+            System.out.print("Abaixo do peso" );
+        } else if (Peso < 18.5 && Peso >  24.9  ){
+            System.out.print("Peso normal" );
+        }else if (Peso < 25.0 && Peso >  29.9  ){
+            System.out.print("Sobrepeso" );
+        }  else if (Peso < 30.0 && Peso >  34.9){
+            System.out.print("Obesidade grau 1" );
+        }else if (Peso < 35.0 && Peso >  39.9){
+            System.out.print("Obesidade grau 2" );
+        }else if (Peso > 40.0 ){
+            System.out.print("Obesidade grau 3 (mórbida)");
+        }
+        
+
+        System.out.printf(" \n O seu peso é: " + Peso + "\n");    
+        System.out.printf("Sua altura  é: " + Altura + "\n");
+        System.out.printf(" \n  " + imc + "\n");
+        
+
 
 }
  }
