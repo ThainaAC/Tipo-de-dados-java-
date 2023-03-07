4518075af097c81d75e2da914af604c11f8228a1
ThainaAC <alvesthaina568@gmail.com>
2023-03-07 14:35:50 +0000

    salvar

---
+++ TarefaTres.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TarefaTres {
+    public static void main(String[] args){
+        Scanner input = new Scanner (System.in);
+        float m, c;
+        
+        System.out.println("Digite um numero: ");
+        m = input.nextfloat();
+
+
+        
+
+
+    }
+}
